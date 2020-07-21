8ed3b762b4681cb7f313371154371feae6dfa28d
Angelina <shabaltas.angel68@gmail.com>
2020-07-21 23:45:11 +0300

    lsn 95: github pages

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
 
     render() {
         return this.props.initialized
-            ? <BrowserRouter>
+            ? <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div className='app-wrapper'>
                     <HeaderContainer/>
                     <NavbarContainer/>
