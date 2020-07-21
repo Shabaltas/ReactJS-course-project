7f249d6398a615f0f34be601f87a81a39d39008a
Angelina <shabaltas.angel68@gmail.com>
2020-07-21 22:37:50 +0300

    Updates

---
+++ precache-manifest.a092f16a1ace75ade7cd0d95fbc7d20b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e0711d2b2cfdc1f7ca8bb8ed644309",
+    "url": "/ReactJS-course-project/index.html"
+  },
+  {
+    "revision": "7d62377ac32cf6a5cd58",
+    "url": "/ReactJS-course-project/static/css/3.d49337a1.chunk.css"
+  },
+  {
+    "revision": "de9597bccc7b238b7d10",
+    "url": "/ReactJS-course-project/static/css/4.2b283795.chunk.css"
+  },
+  {
+    "revision": "e5ee7a085eef5bdc80bd",
+    "url": "/ReactJS-course-project/static/css/5.25b44a53.chunk.css"
+  },
+  {
+    "revision": "86ddaa1bb0883bf2313e",
+    "url": "/ReactJS-course-project/static/css/6.0ab0ac3b.chunk.css"
+  },
+  {
+    "revision": "b5f399e369a8f1923314",
+    "url": "/ReactJS-course-project/static/css/7.d875420a.chunk.css"
+  },
+  {
+    "revision": "dc9551a322237d4ce36e",
+    "url": "/ReactJS-course-project/static/css/main.ee03fd8a.chunk.css"
+  },
+  {
+    "revision": "714e68fa3cbfe2737783",
+    "url": "/ReactJS-course-project/static/js/2.844d35d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-course-project/static/js/2.844d35d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d62377ac32cf6a5cd58",
+    "url": "/ReactJS-course-project/static/js/3.5cec3d75.chunk.js"
+  },
+  {
+    "revision": "de9597bccc7b238b7d10",
+    "url": "/ReactJS-course-project/static/js/4.dc42848d.chunk.js"
+  },
+  {
+    "revision": "e5ee7a085eef5bdc80bd",
+    "url": "/ReactJS-course-project/static/js/5.fe7da95d.chunk.js"
+  },
+  {
+    "revision": "86ddaa1bb0883bf2313e",
+    "url": "/ReactJS-course-project/static/js/6.9ca70cab.chunk.js"
+  },
+  {
+    "revision": "b5f399e369a8f1923314",
+    "url": "/ReactJS-course-project/static/js/7.1160dcc3.chunk.js"
+  },
+  {
+    "revision": "d820aa02a5cc379607c7",
+    "url": "/ReactJS-course-project/static/js/8.b8f28b3e.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/ReactJS-course-project/static/js/8.b8f28b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9551a322237d4ce36e",
+    "url": "/ReactJS-course-project/static/js/main.6685af3d.chunk.js"
+  },
+  {
+    "revision": "85dc7b79577483d4af9b",
+    "url": "/ReactJS-course-project/static/js/runtime-main.98fedd7a.js"
+  },
+  {
+    "revision": "d22f8f3dd8de4c228ca13b9dd50430a0",
+    "url": "/ReactJS-course-project/static/media/ava2.d22f8f3d.png"
+  },
+  {
+    "revision": "63f1670b1f2138d3778af338622f0c0b",
+    "url": "/ReactJS-course-project/static/media/ava3.63f1670b.png"
+  },
+  {
+    "revision": "130d23b3b1ca82e2642f268096fb4867",
+    "url": "/ReactJS-course-project/static/media/contentTop.130d23b3.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactJS-course-project/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6192e41a4b366ebc4eb86d4444c79177",
+    "url": "/ReactJS-course-project/static/media/preloader.6192e41a.gif"
+  }
+]);
---
+++ static/js/6.9ca70cab.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcourse_project_01=this.webpackJsonpcourse_project_01||[]).push([[6],{308:function(t,e,n){},326:function(t,e,n){"use strict";n.r(e);var c=n(0),r=n.n(c);n(308);e.default=function(t){return r.a.createElement("div",null,"Settings")}}}]);
+//# sourceMappingURL=6.9ca70cab.chunk.js.map
